84fb0a40c42d0a215138438e3cbc01eb671f114f
Eason Cheng <cys.eason@gmail.com>
2024-02-14 00:00:04 +0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,8 @@
-# test_calculator.py
+# test_calculator_pytest.py
 
-import unittest
 from calculator import sum_numbers
 
-class TestCalculator(unittest.TestCase):
-
-    def test_sum_numbers(self):
-        self.assertEqual(sum_numbers(5, 7), 12)
-        self.assertEqual(sum_numbers(-1, 1), 0)
-        self.assertEqual(sum_numbers(-1, -1), -2)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_sum_numbers():
+    assert sum_numbers(5, 7) == 12
+    assert sum_numbers(-1, 1) == 0
+    assert sum_numbers(-1, -1) == -2
